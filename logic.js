c46af6f3b6454255b6bb922956499ec79c2544aa
emesefedev <emesefedev@gmail.com>
2024-07-26 14:10:31 +0200

    hover event

--- logic.js
+++ logic.js
@@ -13,9 +13,14 @@ window.addEventListener("load", () => {
     const container = document.querySelector(".flex-container")
     const containerWidth = totalColumns * ELEMENT_SIZE + (totalColumns - 1) * GAP_SIZE
     container.style.width = `${containerWidth}px`
+    container.addEventListener("mouseover", (event) => {
+        const target = event.target
+        target.classList.add("flex-item-hover")
+        container.classList.remove("flex-item-hover")
+    })
 
     for (i = 0; i < totalElements; i++) {
-        const div = document.createElement("div");
+        const div = document.createElement("div")
         div.classList.add("flex-item")
         container.appendChild(div)
     }
