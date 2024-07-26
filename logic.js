27d198995a957e69b8283ffd1fbe79c4dfbf5d55
emesefedev <emesefedev@gmail.com>
2024-07-26 14:01:55 +0200

    display grid

--- logic.js
+++ logic.js
@@ -1,11 +1,24 @@
+const ELEMENT_SIZE = 16 // px
+const GAP_SIZE = 1 //px
+
+const totalRows = 5
+const totalColumns = 4
+
+
+const totalElements = totalRows * totalColumns
+
 window.addEventListener("load", () => {
     console.log("load")
 
-    const cointainer = document.querySelector(".flex-container")
+    const container = document.querySelector(".flex-container")
+    const containerWidth = totalColumns * ELEMENT_SIZE + (totalColumns - 1) * GAP_SIZE
+    container.style.width = `${containerWidth}px`
 
-    const div = document.createElement("div");
-    div.classList.add("flex-item");
-    cointainer.appendChild(div)
+    for (i = 0; i < totalElements; i++) {
+        const div = document.createElement("div");
+        div.classList.add("flex-item")
+        container.appendChild(div)
+    }
     
 })
 
